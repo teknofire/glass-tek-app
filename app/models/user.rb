bd87388b00b20eb217e581c8458741b1930b470c
Will Fisher <will@teknofire.net>
2013-11-30 02:47:38 -0900

    adding credentials needed for mirror api access

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   has_many :authorizations
   
   def self.create_from_hash!(hash)
-    create(name: hash['info']['name'], email: hash['info']['email'])
+    create(
+      name: hash['info']['name'], email: hash['info']['email'], 
+      token: hash['credentials']['token'], refresh_token: hash['credentials']['refresh_token'],
+      expires_at: hash['credentials']['expires_at']
+    )
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131130105357) do
+ActiveRecord::Schema.define(version: 20131130113928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,9 @@ ActiveRecord::Schema.define(version: 20131130105357) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email"
+    t.string   "refresh_token"
+    t.string   "token"
+    t.string   "expires_at"
   end
 
 end
