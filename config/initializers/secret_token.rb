7bdec4f58ae6b150315dbaa10fa043101b3f9bc2
Will Fisher <will@teknofire.net>
2013-11-30 01:07:46 -0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TekApp::Application.config.secret_key_base = 'fadbbd9936ef8a007a01b6597f033afef788d996288278e110e05cd328696f180f69cd2f07760ad13cfea9026bb275eaa441c611055e79e3e4d3dcab0bd20f1e'
